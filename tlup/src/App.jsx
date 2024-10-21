d2d281c85b3ae24c7987462238bca43904894b42
luqas31 <lucas.ers@icloud.com>
2024-10-21 13:21:24 +0100

    tlup working in progress

---
+++ tlup/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className='working'>
+				<img src='../public/images/tlup-logo.png' alt='TiraLaUmaPic Logotipo' />
+				<p>Work in progress...</p>
+				<a href='https://www.instagram.com/tiralaumapic' target='_blank' rel='noopener noreferrer'>
+					<i className='fab fa-instagram'></i>
+				</a>
+			</div>
+		</>
+	);
+}
+
+export default App;
