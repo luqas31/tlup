58d7eb0da631c5d0b3e54f4a819e163537d4b731
luqas31 <lucas.ers@icloud.com>
2024-10-28 15:43:39 +0000

    organized folders

--- tlup/src/components/NavBar/index.jsx
+++ tlup/src/components/NavBar/index.jsx
@@ -1,11 +1,42 @@
-import './index.css'
+import './index.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
 
 function NavBar() {
-  return (
-      <div>
-          asd
-    </div>
-  )
+	const navRef = useRef();
+	const showNavBar = () => {
+		navRef.current.classList.add('responsive_nav');
+	};
+
+	return (
+		<div>
+			<header>
+				<h3>LOGO</h3>
+				<nav ref={navRef}>
+					<ul>
+						<li>
+							<a href='#'>Home</a>
+						</li>
+						<li>
+							<a href='#'>About</a>
+						</li>
+						<li>
+							<a href='#'>Services</a>
+						</li>
+						<li>
+							<a href='#'>Contact</a>
+						</li>
+					</ul>
+				</nav>
+				<button className='nav-btn nav-close-btn' onClick={showNavBar}>
+					<FaTimes />
+				</button>
+				<button className='nav-btn' onClick={showNavBar}>
+					<FaBars />
+				</button>
+			</header>
+		</div>
+	);
 }
 
-export default NavBar
+export default NavBar;
