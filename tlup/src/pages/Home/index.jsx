3854b37fa7ad3a35945ecb7592f38eced41752b5
luqas31 <lucas.ers@icloud.com>
2024-10-29 21:59:55 +0000

    transition photos added

--- tlup/src/pages/Home/index.jsx
+++ tlup/src/pages/Home/index.jsx
@@ -2,11 +2,14 @@ import './index.css';
 import NavBar from '../../components/NavBar';
 import About from '../../sections/About';
 import Contact from '../../sections/Contact';
+import Transition from '../../sections/Transition';
 
 function Home() {
 	return (
 		<div className='bg'>
+			<NavBar />
 			<About />
+			<Transition />
 			<Contact />
 		</div>
 	);
---
+++ tlup/src/sections/Transition/index.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Transition() {
+	return (
+		<>
+			<img className='transition' src='/images/tlup-transition.jpg' alt='' />
+		</>
+	);
+}
+
+export default Transition;
