13f90bd7a600e2c6c468d16872dffbf2d8ca0631
luqas31 <lucas.ers@icloud.com>
2024-11-04 13:04:59 +0000

    added react helmet

--- tlup/src/pages/Home/index.jsx
+++ tlup/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import './index.css';
+import { Helmet } from 'react-helmet';
 import NavBar from '../../components/NavBar';
 import About from '../../sections/About';
 import Contact from '../../sections/Contact';
@@ -9,6 +10,14 @@ import Footer from '../../components/Footer';
 function Home() {
 	return (
 		<div className='bg'>
+			<Helmet>
+				<title>Home - Meu Portfólio</title>
+				<meta
+					name='description'
+					content='Transformamos ideias em experiências audiovisuais memoráveis. Nossa empresa de produção audiovisual oferece serviços de filmagem, edição, animação e criação de conteúdo para empresas, eventos e campanhas publicitárias. Explore nossa paixão pela narrativa visual e leve sua comunicação para o próximo nível.'
+				/>
+				<meta name='keywords' content='tlup, tiralaumapic, TLUP, TiraLaUmaPic, Produtora, Audiovisual, Lisboa, Videoclips, Fotografia'/>
+			</Helmet>
 			<NavBar />
 			<main>
 				<About />
