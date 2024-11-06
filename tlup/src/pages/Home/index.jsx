2ca3fcc61f01f01a3d68f55862e2bb3bad142449
luqas31 <lucas.ers@icloud.com>
2024-11-06 17:02:01 +0000

    responsive contact done

--- tlup/src/pages/Home/index.jsx
+++ tlup/src/pages/Home/index.jsx
@@ -3,34 +3,31 @@ import { Helmet } from 'react-helmet';
 import NavBar from '../../components/NavBar';
 import About from '../../sections/About';
 import Contact from '../../sections/Contact';
-import Contact2 from '../../sections/Contact/contact';
 import Transition from '../../sections/Transition';
 import Works from '../../sections/Works';
 import Footer from '../../components/Footer';
 
 function Home() {
 	return (
-		<div className='bg'>
+		<>
 			<Helmet>
 				<title>TLUP | Produtora Audiovisual</title>
 				<meta
 					name='description'
 					content='Transformamos ideias em experiências audiovisuais memoráveis. Nossa empresa de produção audiovisual oferece serviços de filmagem, edição, animação e criação de conteúdo para empresas, eventos e campanhas publicitárias. Explore nossa paixão pela narrativa visual e leve sua comunicação para o próximo nível.'
 				/>
-				<meta name='keywords' content='tlup, tiralaumapic, TLUP, TiraLaUmaPic, Produtora, Audiovisual, Lisboa, Videoclips, Fotografia'/>
+				<meta name='keywords' content='tlup, tiralaumapic, TLUP, TiraLaUmaPic, Produtora, Audiovisual, Lisboa, Videoclips, Fotografia' />
 			</Helmet>
 			<NavBar />
 			<main>
 				<About />
 				<Transition />
 				<Works />
-				<Contact2 />
-				
-		{/* 	
-				<Contact /> */}
+				<Contact />
 			</main>
+
 			<Footer />
-		</div>
+		</>
 	);
 }
 
--- tlup/src/sections/Contact/index.jsx
+++ tlup/src/sections/Contact/index.jsx
@@ -1,48 +1,39 @@
+import React from 'react';
 import './index.css';
 
 function Contact() {
 	return (
-		<div>
-			<section id='contact' className='contact'>
-				<div className='contact-container'>
-					<h1>contactos</h1>
-					<div className='contact-content'>
-						<div className='contact-name-email'>
-							<div className='input-container contact-name'>
-								<input type='text' placeholder=' ' />
-								<label>Nome</label>
+		<>
+			<section className='works-section'>
+				<div className='section-bg'>
+					<div className='section-title'>
+						<h2>vem trabalhar connosco</h2>
+					</div>
+					<div className='section-content '>
+						<div className='contact-content'>
+							<div className='contact-name-email'>
+								<div className='input-container contact-name'>
+									<input type='text' placeholder=' ' />
+									<label>Nome</label>
+								</div>
+								<div className='input-container contact-subject'>
+									<input type='text' placeholder=' ' />
+									<label>Assunto</label>
+								</div>
 							</div>
-							<div className='input-container contact-subject'>
-								<input type='text' placeholder=' ' />
-								<label>Assunto</label>
+							<div className='input-container contact-email'>
+								<input type='email' placeholder=' ' />
+								<label>O teu endereço de email</label>
+							</div>
+							<div className='input-container textarea-container contact-message'>
+								<textarea placeholder=' '></textarea>
+								<label>A tua mensagem</label>
 							</div>
 						</div>
-						<div className='input-container contact-email'>
-							<input type='email' placeholder=' ' />
-							<label>O teu endereço de email</label>
-						</div>
-						<div className='input-container textarea-container contact-message'>
-							<textarea placeholder=' '></textarea>
-							<label>A tua mensagem</label>
-						</div>
-					</div>
-					<div className='contact-button'>
-						<button className='button-55' role='button'>
-							ENVIAR
-						</button>
-					</div>
-					<div className='contact-or-email'>
-						<p>ou</p>
-					</div>
-					<div className='contact-alternatives'>
-						<p>Tel: +351 123 456 789</p>
-						<p>Tel: +351 123 456 789</p>
-						<p>tlup@tlup.pt</p>
 					</div>
 				</div>
 			</section>
-			
-		</div>
+		</>
 	);
 }
 
