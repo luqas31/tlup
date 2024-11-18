6602faf76ae1865fc66254482e08b9959337c906
luqas31 <lucas.ers@icloud.com>
2024-11-18 17:01:04 +0000

    Estilização feita

--- tlup/src/sections/AlbumComp/index.jsx
+++ tlup/src/sections/AlbumComp/index.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './index.css';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 
 function AlbumComp() {
 	const { documentId } = useParams();
 	const [album, setAlbum] = useState(null);
+
+    const navigate = useNavigate();
+
 
 	useEffect(() => {
 		const API_BASE_URL = 'http://admin.tlup.pt/api';
@@ -27,22 +32,38 @@ function AlbumComp() {
 			<section className='album'>
 				<div className='section-bg' id='pfolio'>
 					<div className='section-title'>
-						<h2>{album ? album.data.nome : 'Carregando...'}</h2>
+						<h2>{album ? album.data.nome : 'Album desconhecido'}</h2>
 					</div>
-					<div className='pfolio-filter'>
-						<button>Voltar</button>
+					<div className='pfolio-filter' id='voltar'>
+						<button className='filter-item' onClick={() => navigate(-1)}>
+							<FaArrowLeft style={{ marginRight: '8px' }} />
+							Voltar a albums
+						</button>
 					</div>
 					<div className='section-content'>
 						<div className='pfolio-grid'>
-							{album && album.data.fotos ? (
-								album.data.fotos.map((foto, index) => (
-									<div key={index} className='pfolio-item'>
-										<img src={`http://admin.tlup.pt${foto.url}`} alt={`foto-${index}`} />
-									</div>
-								))
-							) : (
-								<p>Sem fotos disponíveis.</p>
-							)}
+							{album && album.data.fotos
+								? album.data.fotos.map((foto, index) => (
+										<div key={index} className='pfolio-item'>
+											<img src={`http://admin.tlup.pt${foto.url}`} alt={`foto-${index}`} />
+										</div>
+								  ))
+								: null}
+							{album && album.data.videos.length > 0
+								? album.data.videos.map((video, index) => (
+										<div key={index} className='pfolio-item'>
+											<iframe
+												width='100%'
+												height='100%'
+												src={`https://www.youtube.com/embed/${video.link}`}
+												title={`YouTube video player ${index}`}
+												frameBorder='0'
+												allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+												allowFullScreen
+											></iframe>
+										</div>
+								  ))
+								: null}
 						</div>
 					</div>
 				</div>
